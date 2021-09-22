8caa163db27e65920b63d6d3f7dcc298d5893245
Kumar Divyanshu <divyanshu9707@gmail.com>
2021-09-22 06:58:29 +0530

    Implemented React hooks - useState, useEffect, useReducer

---
+++ src/components/hooks/useEffect.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+
+const UseEffect = () => {
+  const [myNum, setMyNum] = useState(0);
+
+  useEffect(() => {
+      document.title = `Chats(${myNum})`
+  })
+  
+    return (
+        <>
+          <div className="center_div">
+            <p>{myNum}</p>
+            <div class="button2" onClick={() => setMyNum(myNum + 1)}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              INCR
+            </div>
+          </div>
+        </>
+    );
+}
+
+export default UseEffect;
